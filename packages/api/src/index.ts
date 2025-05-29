05d12de77653678bce968e7e657bddde1133fe56
m1ggy <miguel.buising1@gmail.com>
2025-05-29 22:20:39 +0800

    remove cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -23,9 +23,6 @@ const app = Fastify({
 
 app.register(fastifySocketIO, {
   path: '/ws',
-  cors: {
-    origin: ['http://localhost:5173', 'https://stagingspace.org'],
-  },
 });
 app.register(fastifyCors, {
   origin: ['http://localhost:5173', 'https://stagingspace.org'],
