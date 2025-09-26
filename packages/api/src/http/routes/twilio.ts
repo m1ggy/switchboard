e33260b6c4342efd8872bee6a74f2c51eff7ff11
m1gs <miguel.buising1@gmail.com>
2025-09-27 00:31:31 +0800

    remove pause

--- packages/api/src/http/routes/twilio.ts
+++ packages/api/src/http/routes/twilio.ts
@@ -117,7 +117,6 @@ async function routes(app: FastifyInstance) {
             method: 'POST',
             actionOnEmptyResult: true,
           })
-          .pause({ length: 1 })
           .say(
             { voice: 'Polly.Joanna', language: 'en-US' },
             `Thank you for calling ${companyName}. Press 1 to speak with an agent. Or, if you'd like to send a fax, please stay on the line.`
